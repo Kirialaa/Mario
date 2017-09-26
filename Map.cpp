0ed0af24d04314fbdc37cf35e48277a8b296f6b0
Kirialaa <Daw2300@o2.pl>
2017-09-26 11:20:27 +0200

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -2,10 +2,26 @@
 #include "Map.h"
 
 using namespace std;
-
-void Map::generate_map(int szerokosc, int wysokosc)
-{
-
-
+int i=0, j=0, b=0;
+bool Map::generate_map(int szerokosc, int wysokosc)
+{     //generowanie pierwszej czesci
+  while(i<szerokosc)
+  {
+  cout <<"-";
+  }
+  cout <<" "<<endl;
+  //koniec generowania pierwszej czesci
+  while(j<wysokosc && b<wysokosc )
+  {
+    cout <<"|"
+      i=0;
+      while(i<szerokosc)
+      {
+        cout <<" ";
+      }
+      cout <<"|"<<endl;
+    j=0;
+    b++;
+  }
 
 }
