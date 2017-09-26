c799871e59d7c53ca4903cc2e8ca328fc1f39c77
Kirialaa <Daw2300@o2.pl>
2017-09-26 11:36:00 +0200

    Update Core.cpp

--- Core.cpp
+++ Core.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 #include "Core.h"
+
 using namespace std;
+
+float Core::get_lokalizacja(char lokalizacja[250][250], int dlugosc)
+{
+  bool loc[250][250]//pierwsza wspolrzedna to X, druga Y;
+  int i=0, f=0;
+  int x[1000], x_tracking=0;
+  int y[1000]=0, y_tracking=0;
+  while(i<=dlugosc)
+  {
+      while(lokalizacja[x][y]!=219)//219 to waz
+      {
+        loc[x_tracking][y_tracking]=false;
+      }
+    if(lokalizacja[x][y]==219)
+     lokalizacja[x_tracking][y_tracking]=true;
+      x[f][f]=//Trzeba dopisac
+  }
+ }
+
+
+
+
+void Core::kolizja()
+{
+  Core::get_lokalizacja(int ID)
+    if()
+}
